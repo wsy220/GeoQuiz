06f262ba0e0d4e50bcc4a9cc11f0b32331a90322
wsy <35551400@qq.com>
2018-03-17 10:46:42 +0800

    dddd

--- app/src/main/java/com/bignerdranch/andriod/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/andriod/geoquiz/QuizActivity.java
@@ -27,6 +27,7 @@ public class QuizActivity extends AppCompatActivity {
         mFalseButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 Toast.makeText(QuizActivity.this,R.string.false_toast,Toast.LENGTH_SHORT).show();
+
             }
         });
     }
