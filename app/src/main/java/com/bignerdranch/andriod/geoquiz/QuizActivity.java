d488d9b44886a92acd11c4352f1a1da255dd3240
wsy <35551400@qq.com>
2018-03-17 10:45:30 +0800

    dddd

---
+++ app/src/main/java/com/bignerdranch/andriod/geoquiz/QuizActivity.java
@@ -0,0 +1,33 @@
+package com.bignerdranch.andriod.geoquiz;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class QuizActivity extends AppCompatActivity {
+    private Button mTrueButton;
+    private Button mFalseButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+        mTrueButton=(Button)findViewById(R.id.true_button);
+        mFalseButton=(Button)findViewById(R.id.false_button);
+
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this,R.string.true_toast,Toast.LENGTH_SHORT).show();
+            }
+        });
+        mFalseButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Toast.makeText(QuizActivity.this,R.string.false_toast,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
