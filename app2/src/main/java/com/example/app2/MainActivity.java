e28dcb7d50067a7c1a815dcc127537c0b9315dd9
35551400@qq.com <fish1981220>
2018-08-15 23:04:55 +0800

    fffdfgfsdfsd

--- app2/src/main/java/com/example/app2/MainActivity.java
+++ app2/src/main/java/com/example/app2/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.app2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DigitalClock;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,12 +15,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button button = findViewById(R.id.btn_as);
+        final DigitalClock digitalClock = findViewById(R.id.digitaClock);
         button.setOnClickListener(new View.OnClickListener() {
-                                      @Override
-          public void onClick(View view) {
-
-          }
-      }
-        );
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, R.string.true_toast, Toast.LENGTH_SHORT).show();
+                digitalClock.setVisibility(View.GONE);
+            }
+        });
     }
 }
