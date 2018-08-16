d680646bc9a38eadaeaa2f5e5db6ade90e990cb3
wsy <35551400@qq.com>
2018-08-16 16:21:18 +0800

    更新app

--- app/src/main/java/com/bignerdranch/andriod/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/andriod/geoquiz/QuizActivity.java
@@ -22,7 +22,6 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(QuizActivity.this,R.string.true_toast,Toast.LENGTH_SHORT).show();
-
             }
         });
         mFalseButton.setOnClickListener(new View.OnClickListener(){
